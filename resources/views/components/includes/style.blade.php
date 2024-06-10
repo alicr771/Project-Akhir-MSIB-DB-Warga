d2a1cca7da7ddb42482db6deba774143c3c10b46
haidaryuum <haidar.alhazen93@gmail.com>
2024-06-10 22:11:29 +0700

    fixing bootstrap

--- resources/views/components/includes/style.blade.php
+++ resources/views/components/includes/style.blade.php
@@ -1,6 +1,6 @@
 <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-<link href="/css/nucleo-icons.css" rel="stylesheet" />
-<link href="/css/nucleo-svg.css" rel="stylesheet" />
-<link rel="stylesheet" href="/css/argon-dashboard.css">
+<link href="{{ asset('css/nucleo-icons.css') }}" rel="stylesheet" />
+<link href="{{ asset('css/nucleo-svg.css') }}" rel="stylesheet" />
+<link rel="stylesheet" href="{{ asset('css/argon-dashboard.css') }}">
 @vite('resources/sass/app.scss', 'resources/js/app.js')
