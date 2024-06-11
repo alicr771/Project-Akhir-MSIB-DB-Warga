2f2d02cf502b2bfbbb20a00375fcd9703ca17570
NrJulianto <ja921081@gmail.com>
2024-06-11 11:51:30 +0700

    layout:forgot-password&reset-password

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::view('/forgot-password', 'auth.forgot-password')->name('forgot.password');
+
+Route::view('/reset-password', 'auth.reset-password')->name('reset.password');
