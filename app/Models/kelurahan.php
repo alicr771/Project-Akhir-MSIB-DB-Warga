b2bc37df812827080790462d955a7d66685903e1
Adityadwicahyo23 <masadidbow23@gmail.com>
2024-06-19 10:35:13 +0700

    create migrations,model,controller kelurahan By Adit

---
+++ app/Models/kelurahan.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class kelurahan extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'head'
+    ];
+
+    public function penduduks()
+    {
+        return $this->hasMany(Penduduk::class, 'kelurahan_id', 'id_kelurahan');
+    }
+}
